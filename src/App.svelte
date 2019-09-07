d64128f0bcfbf3da1f33d612debbb5535df037d1
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2019-09-07 14:39:52 -0400

    before trying routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,48 @@
+<main>
+    <svelte:component this={Route} {params} />
+</main>
+
 <script>
-	let name = 'world';
+	import Navaid from 'navaid';
+    import Header from './Header.svelte';
+    import { onDestroy } from 'svelte';
+    let Route, params, active;
+    let uri = location.pathname;
+    $: active = uri.split('/')[1] || 'home';
+    function draw(m, obj) {
+        params = obj || {};
+        if (m.preload) {
+            m.preload({ params }).then(() => {
+                Route = m.default;
+            });
+        } else {
+            Route = m.default;
+        }
+    }
+    function track(obj) {
+        uri = obj.state || obj.uri;
+        if (window.ga) ga.send('pageview', { dp:uri });
+    }
+    addEventListener('replacestate', track);
+    addEventListener('pushstate', track);
+    addEventListener('popstate', track);
+    const router = Navaid('/')
+        .on('/', () => import('../routes/Home.svelte').then(draw))
+        .on('/about', () => import('../routes/About.svelte').then(draw))
+        .on('/blog', () => import('../routes/Blog.svelte').then(draw))
+        .on('/blog/:title', obj => import('../routes/Article.svelte').then(m => draw(m, obj)))
+        .listen();
+    onDestroy(router.unlisten);
 </script>
 
-<h1>Hello {name}!</h1>
+
+</script>
+<div class="header-container">
+    <button class="title-button">Kaelan Moffett-Steinke</button>
+    <div class="menu-container">
+        <button class="menu-buttons">Home</button>
+        <button class="menu-buttons">Portfolio</button>
+        <button class="menu-buttons">Contact</button>
+    </div>
+</div>
+
---
+++ src/Header.svelte
@@ -0,0 +1,73 @@
+<style type="text/css">
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .header-container{
+        display:flex;
+        /*margin:auto;*/
+        margin-left: 5%;
+        margin-right: 5%;
+        justify-content: space-between;
+    }
+
+    .title-button{
+        color:black;
+        text-transform: uppercase;
+        font-size: 4vw;
+        background-color: Transparent;
+        background-repeat: no-repeat;
+        border: none;
+        cursor:pointer;
+        overflow: hidden;
+        outline:none;
+    }
+
+    .title-button:hover{
+        color:gray;
+        text-transform: uppercase;
+        font-size: 4vw;
+        background-color: Transparent;
+        background-repeat: no-repeat;
+        border: none;
+        cursor:pointer;
+        overflow: hidden;
+        outline:none;
+    }
+
+    .menu-container{
+        display: flex;
+        /*margin-left: auto;*/
+        justify-content: flex-end;
+    }
+
+    .menu-buttons{
+        padding: 15px;
+        background-color: Transparent;
+        background-repeat:no-repeat;
+        border: none;
+        cursor:pointer;
+        overflow: hidden;
+        outline:none;
+    }
+</style>
+
+<script>
+    export let selected = 'home';
+
+    //function menuButtonHandler(name){
+
+    //}
+
+</script>
+
+<div class="header-container">
+    <button class="title-button">Kaelan Moffett-Steinke</button>
+    <div class="menu-container">
+        <button class="menu-buttons">Portfolio</button>
+        <button class="menu-buttons">Contact</button>
+        <button class="menu-buttons">Other</button>
+    </div>
+</div>
